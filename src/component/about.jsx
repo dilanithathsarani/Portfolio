b26dcd58a58f6dbb007ce807d77eb2c543f22f0e
Dilani <dilanithathsarani2003@gmail.com>
2025-09-05 22:03:01 +0530

    education update for md,sm screen

--- src/component/about.jsx
+++ src/component/about.jsx
@@ -147,10 +147,11 @@ export default function About() {
       Education
     </motion.h2>
 
-    {/* Vertical Timeline Line */}
-    <div className="absolute left-1/2 transform -translate-x-1/2 top-10 h-full w-1 
-                bg-gradient-to-b from-[#3A9188]/80 via-[#B8E1DD]/40 to-[#3A9188]/80 
-                rounded shadow-lg hidden lg:block"></div>
+{/* Vertical Timeline Line */}
+<div className="hidden lg:block absolute left-1/2 transform -translate-x-1/2 top-10 
+                h-full w-1 bg-gradient-to-b from-[#3A9188]/80 via-[#B8E1DD]/40 
+                to-[#3A9188]/80 rounded shadow-lg"></div>
+
 
     {/* Timeline Cards */}
     <div className="flex flex-col space-y-8 md:space-y-12">
