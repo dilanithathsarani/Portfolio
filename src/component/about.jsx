64ed45eb32d93df912c7b4b5be25e20cdef74bd5
Dilani <dilanithathsarani2003@gmail.com>
2025-09-06 13:06:45 +0530

    image position change

--- src/component/about.jsx
+++ src/component/about.jsx
@@ -1,16 +1,48 @@
 import { motion } from "framer-motion";
 import profileImg from "../assets/about.jpg";
-import { FaArrowRight } from "react-icons/fa";
+import { FaArrowRight, FaSchool, FaGraduationCap } from "react-icons/fa";
 
 export default function About() {
+  const education = [
+    {
+       level: "Undergraduate",
+      details: "BSc (Hons) in Computing & Information Systems",
+      year: "Sabaragamuwa University of Sri Lanka",
+      time:"2024 - Present",
+      icon: <FaGraduationCap className="text-[#3A9188] w-5 h-5 animate-pulse" />,
+    },
+    {
+      level: "Advanced Level (A/L) – Mathematical Stream",
+      details: "Results: ABS",
+      year: "G/Anula Devi Balika Vidyalaya, Galle",
+      time:"2020 - 2022",
+      icon: <FaSchool className="text-[#3A9188] w-5 h-5 animate-pulse" />,
+    },
+    {
+      level: "Ordinary Level (O/L)",
+      details: "Results: A4, B5",
+      year: "G/Anula Devi Balika Vidyalaya, Galle",
+      time:"2019",
+      icon: <FaSchool className="text-[#3A9188] w-5 h-5 animate-pulse" />,
+    },
+    {
+      level: "Certificate In Informaion Technology",
+      details: "Result: A",
+      year: "Nenaiyasa - Rular Education Development & Vocational Training Institute",
+      time:"2020 Jan - 2020 Sep",
+      icon: <FaSchool className="text-[#3A9188] w-5 h-5 animate-pulse" />,
+    },
+  ];
+
   return (
     <section
       id="about"
       className="min-h-screen flex items-center justify-center bg-[#044A42] px-4 py-20 overflow-x-hidden"
     >
       <div className="max-w-6xl mx-auto grid md:grid-cols-2 gap-12 items-center">
+
         <motion.div
-          className="flex justify-center relative"
+          className="flex justify-center relative "
           initial={{ opacity: 0, scale: 0.6, y: 50 }}
           whileInView={{ opacity: 1, scale: 1, y: 0 }}
           transition={{ duration: 0.8, type: "spring" }}
@@ -19,7 +51,7 @@ export default function About() {
           <motion.img
             src={profileImg}
             alt="Dilani Profile"
-            className="w-90 h-90 md:w-100 md:h-100 object-cover rounded-full border-4 border-[#3A9188] shadow-[0_0_80px_#3A9188]"
+            className="w-90 h-90 md:w-100 md:h-100 object-cover rounded-full border-4 border-[#3A9188] shadow-[0_0_80px_#3A9188] mb-85"
             animate={{ y: [0, -12, 0] }}
             transition={{ duration: 4, repeat: Infinity, ease: "easeInOut" }}
           />
@@ -32,7 +64,7 @@ export default function About() {
         </motion.div>
 
         <motion.div
-          className="text-center md:text-left"
+          className="text-center md:text-left order-2 md:order-2"
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true }}
@@ -75,6 +107,29 @@ export default function About() {
                 )}
             </motion.p>
           ))}
+
+          <motion.div className="mb-6 mt-6">
+            <h3 className="text-2xl font-semibold text-[#B8E1DD] mb-4"> Education</h3>
+            <ul className="space-y-6">
+              {education.map((edu, idx) => (
+                <motion.li
+                  key={idx}
+                  className="flex items-start gap-4"
+                  initial={{ opacity: 0, x: -20 }}
+                  whileInView={{ opacity: 1, x: 0 }}
+                  transition={{ duration: 0.6, delay: idx * 0.2 }}
+                >
+                  <span>{edu.icon}</span>
+                  <div>
+                    <p className="font-semibold text-[#B8E1DD]">{edu.level}</p>
+                    <p className="text-[#B8E1DD]/90">{edu.details}</p>
+                    <p className="text-[#B8E1DD]/70 text-sm">{edu.year}</p>
+                    <p className="text-[#B8E1DD]/70 text-sm">{edu.time}</p>
+                  </div>
+                </motion.li>
+              ))}
+            </ul>
+          </motion.div>
 
           <motion.a
             href="#contact"
